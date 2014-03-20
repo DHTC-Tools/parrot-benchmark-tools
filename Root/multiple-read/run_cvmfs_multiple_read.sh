9b5472e354448ba8e50e30aedbe942e2b7c97511
Suchandra Thapa <sthapa@login01.osgconnect.net>
2014-03-20 12:41:32 -0500

    Commit multiple read benchmark files

---
+++ Root/multiple-read/run_cvmfs_multiple_read.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+/bin/hostname
+export TMP_DIR="$OSG_WN_TMP"
+work_dir=`mktemp -d -t root.XXXXXXXXX`
+if [ ! -d $work_dir ]; then
+  echo "Can't create work dir"
+  exit 1
+fi
+start_dir=`pwd`
+cd $work_dir
+start_time=`date +"%s.%N"`
+wget http://stash.osgconnect.net/+sthapa/cvmfs-root.tar.gz
+download_time=`date +"%s.%N"`
+export PATH=$PATH:$work_dir/cvmfs-root/
+tar xvzf cvmfs-root.tar.gz
+cd cvmfs-root
+exec_time=`date +"%s.%N"`
+ls
+time $work_dir/cvmfs-root/read-multiple.sh 
+end_time=`date +"%s.%N"`
+rm -fr $work_dir
